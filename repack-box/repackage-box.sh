f6f1d736e18ab9ec697b55776e0ec1cfc281b038
GTaf <auredu94@gmail.com>
2018-06-20 16:18:25 -0700

    Fixed box repacking script

--- repack-box/repackage-box.sh
+++ repack-box/repackage-box.sh
@@ -1,5 +1,5 @@
 # change the Vagrantfile to suppress ssh issues 
-./repackage/suppress-ssh-warning.sh
+#./repackage/suppress-ssh-warning.sh
 
 # boot up VM, provisioning to install camflow in the kernel
 vagrant up
@@ -9,4 +9,4 @@ vagrant halt
 vagrant up
 vagrant ssh -c '/vagrant/repackage/clean-up.sh'
 
-vagrant package --base CamFlow-light
+vagrant package --base CamFlow-rpm
